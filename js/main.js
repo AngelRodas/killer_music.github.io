f15c663f31e9b721df575c0fd6d5e4686a13ce9a
AngelRodas <rodassocorecangelrene@gmail.com>
2024-03-27 17:36:11 -0600

    se adapta para cualquier dispositivo y se agregan botones para pantallas pequeñas

--- js/main.js
+++ js/main.js
@@ -2,15 +2,40 @@ const cloud = document.getElementById("cloud");
 const barraLateral = document.querySelector(".barra-lateral");
 const spans = document.querySelectorAll("span");
 const mo=document.querySelector(".switch");
+const circulo = document.querySelector(".circulo")
+const menu=document.querySelector(".menu");
+const main= document.querySelector(".main")
+menu.addEventListener("click",()=>{
+    barraLateral.classList.toggle("max-barra-lateral");
+    if(barraLateral.classList.contains("max-barra-lateral")){
+        menu.children[0].style.display="none";
+        menu.children[1].style.display="block"
+    }
+    else
+    {
+        menu.children[0].style.display="block";
+        menu.children[1].style.display="none"
+    }
+    if(window.innerWidth<=300){
+        barraLateral.classList.toggle("mini-barra-lateral");
+        main.classList.add("min-main");
+        spans.forEach((span)=>{
+            spans.classList.add("oculto");
+        })
+    }
+});
 
 mo.addEventListener("click",()=>{
   let body =document.body;
   body. classList.toggle("dark-mode")   
+  circulo.classList.toggle("prendido");
 })
 
 cloud.addEventListener("click",()=>{
 
 barraLateral.classList.toggle("mini-barra-lateral");
+main.classList.toggle("min-main");
+
 spans.forEach((span)=>{
     span.classList.toggle("oculto");
 })

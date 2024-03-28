50ae9ee77a1f6e9c25a1d804b2bd0c2dcda84be6
AngelRodas <rodassocorecangelrene@gmail.com>
2024-03-27 19:48:57 -0600

    se repara y se cierra el div

--- js/main.js
+++ js/main.js
@@ -1,43 +1,39 @@
 const cloud = document.getElementById("cloud");
 const barraLateral = document.querySelector(".barra-lateral");
 const spans = document.querySelectorAll("span");
-const mo=document.querySelector(".switch");
-const circulo = document.querySelector(".circulo")
-const menu=document.querySelector(".menu");
-const main= document.querySelector(".main")
-menu.addEventListener("click",()=>{
+const mo = document.querySelector(".switch");
+const circulo = document.querySelector(".circulo");
+const menu = document.querySelector(".menu");
+const main = document.querySelector("main");
+
+menu.addEventListener("click", () => {
     barraLateral.classList.toggle("max-barra-lateral");
-    if(barraLateral.classList.contains("max-barra-lateral")){
-        menu.children[0].style.display="none";
-        menu.children[1].style.display="block"
+    if (barraLateral.classList.contains("max-barra-lateral")) {
+        menu.children[0].style.display = "none";
+        menu.children[1].style.display = "block";
+    } else {
+        menu.children[0].style.display = "block";
+        menu.children[1].style.display = "none";
     }
-    else
-    {
-        menu.children[0].style.display="block";
-        menu.children[1].style.display="none"
-    }
-    if(window.innerWidth<=300){
-        barraLateral.classList.toggle("mini-barra-lateral");
+    if (window.innerWidth <= 320) {
+        barraLateral.classList.add("mini-barra-lateral");
         main.classList.add("min-main");
-        spans.forEach((span)=>{
-            spans.classList.add("oculto");
-        })
+        spans.forEach((span) => {
+            span.classList.add("oculto");
+        });
     }
 });
 
-mo.addEventListener("click",()=>{
-  let body =document.body;
-  body. classList.toggle("dark-mode")   
-  circulo.classList.toggle("prendido");
-})
+mo.addEventListener("click", () => {
+    let body = document.body;
+    body.classList.toggle("dark-mode");
+    circulo.classList.toggle("prendido");
+});
 
-cloud.addEventListener("click",()=>{
-
-barraLateral.classList.toggle("mini-barra-lateral");
-main.classList.toggle("min-main");
-
-spans.forEach((span)=>{
-    span.classList.toggle("oculto");
-})
-
+cloud.addEventListener("click", () => {
+    barraLateral.classList.toggle("mini-barra-lateral");
+    main.classList.toggle("min-main");
+    spans.forEach((span) => {
+        span.classList.toggle("oculto");
+    });
 });

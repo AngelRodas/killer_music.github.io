942a6d07b57fca4df54be44b762eb6731072db5c
jose4to <josegran6741@gmail.com>
2024-05-20 23:42:45 -0600

    a

--- album.php
+++ album.php
@@ -10,9 +10,15 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"/>
     <link rel="stylesheet" href="includes/css/main.css">
+    <link rel="stylesheet" href="includes/css/boton.css">
     <title>Document</title>
 </head>
 <body>
+    <div class="posicion">
+        <div class="player__btn player__btn--small" id="previous"  >
+            <a href="musica.php"><i class="fas fa-arrow-left"></i></a>
+        </div>
+    </div>
     <div class="container">
 	    <input type="text" placeholder="Buscar">
 		<div class="btn">

b62bf4c74c9e96395d2cf2491e5055ecc5e17b10
jose4to <josegran6741@gmail.com>
2024-05-20 22:44:39 -0600

    -

--- agregar.php
+++ agregar.php
@@ -1,5 +1,6 @@
 <?php
 require_once "includes/header.php";
+ $conn = Cconexion::ConexionDB();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -54,9 +55,45 @@ require_once "includes/header.php";
     </style>
 </head>
 <body>
+    <?php
+    if(isset($_POST['agregar'])){
+        $nombre = $_POST['nombre'];
+        $artista = $_POST['artista'];
+        $album = $_POST['album'];        
+        $archivo = $_POST['archivo'];
+        $duracion = $_POST['duracion'];
+    
+
+        if(strlen($nombre)>0 && strlen($album)>0  && strlen($archivo)>0){
+            $options = array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL);
+            $query = "SELECT AlbumID, Nombre FROM Album";
+            $selectAlbum = $conn->prepare($query,$options);
+            if($selectAlbum->execute()){
+                while($row = $selectAlbum->fetch(PDO::FETCH_ASSOC)){
+                    if($row['Nombre']==$album){                  
+                        $album = $row['AlbumID'];     
+                        $options2 = array( PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY, PDO::SQLSRV_ATTR_QUERY_TIMEOUT => 1);
+                        $insertCancion = $conn->prepare("INSERT INTO Cancion(Nombre, Archivo, AlbumID, Duracion) values (?,?,?,?)", $options2);
+        
+                        if($insertCancion->execute(array($nombre, "audio/$archivo", $album, $duracion))) {
+                        echo "Cancion registrado exitósamente";
+                        
+                        } else {
+                            echo "Error: " . $insertCancion->error;
+                        }            
+                        unset($insertCancion);
+                        unset($conn);
+                    }
+                }                                                                   
+            }
+            
+            
+        }
+    }
+    ?>
 <div class="container mt-5">
-        <h2 class="text-center mb-4">Formulario de Música</h2>
-        <form action="conexion.php" method="POST">
+        <h2 class="text-center mb-2">Formulario de Música</h2>
+        <form action="" method="POST">
             <div class="row mb-3">
                 <div class="col-md-6">
                     <label for="nombre" class="form-label fw-bold">Nombre de la Canción:</label>
@@ -64,7 +101,7 @@ require_once "includes/header.php";
                 </div>
                 <div class="col-md-6">
                     <label for="artista" class="form-label fw-bold">Artista:</label>
-                    <input type="text" class="form-control" id="artista" name="artista" required>
+                    <input type="text" class="form-control" id="artista" name="artista" >
                 </div>
             </div>
             <div class="row mb-3">
@@ -73,25 +110,38 @@ require_once "includes/header.php";
                     <input type="text" class="form-control" id="album" name="album" required>
                 </div>
                 <div class="col-md-6">
-                    <label for="genero" class="form-label fw-bold">Género:</label>
-                    <input type="text" class="form-control" id="genero" name="genero" required>
+                    <label for="genero" class="form-label fw-bold">Archivo MP3:</label>
+                    <input type="text" class="form-control" id="genero" name="archivo" required>
                 </div>
             </div>
             <div class="row mb-3">
                 <div class="col-md-6">
-                    <label for="imagen" class="form-label fw-bold">Imagen: </label>
-                    <input type="text" class="form-control" id="imagen" name="imagen" required>
+                    <label for="imagen" class="form-label fw-bold">Genero: </label>
+                    <input type="text" class="form-control" id="imagen" name="Genero">
                 </div>
                 <div class="col-md-6">
-                    <label for="anio" class="form-label fw-bold">Año de Lanzamiento:</label>
-                    <input type="number" class="form-control" id="anio" name="anio" required>
+                    <label for="anio" class="form-label fw-bold">Duracion:</label>
+                    <input type="number" class="form-control" id="duracion" name="duracion" required>
                 </div>
             </div>
             <div class="text-center">
-                <button type="submit" class="btn btn-primary">Agregar</button>
+                <button type="submit" class="btn btn-primary" name="agregar">Agregar</button>
+            </div>
+        </form>
+            <br>
+            <div class="">
+                <a href=""><button class=" btn-primary" name="agregar">Agregar Género</button></a>
+            </div>
+            <br>
+            <div class="">
+                <a href=""><button class=" btn-primary" name="agregar">Agregar Artísta</button></a>
+            </div>
+            <br>
+            <div class="">
+                <a href=""><button class=" btn-primary">Agregar Album</button></a>
             </div>
             
-        </form>
+        
 
     </div>
 </div>
